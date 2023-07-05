eb60f4c0c282372366112771b0c4728c1d6a49fc
rshvraj <33702025+rshvraj@users.noreply.github.com>
2023-07-05 12:01:46 +0530

    payment branch creation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,11 +11,12 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import Payments from './components/Payments/Payments';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+      {/* <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
@@ -34,7 +35,8 @@ function App() {
             </Link>
           </VStack>
         </Grid>
-      </Box>
+      </Box> */}
+      <Payments/>
     </ChakraProvider>
   );
 }
