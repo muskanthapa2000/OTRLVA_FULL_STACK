6f7d9b068bedc0c35626be6ea5478b3e4ac957c6
rshvraj <33702025+rshvraj@users.noreply.github.com>
2023-07-06 19:20:45 +0530

    card-layout

--- my-app/src/components/Payments/Payments.jsx
+++ my-app/src/components/Payments/Payments.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
 
-const Payments = () => {
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Card,
+  Image,
+  Stack,
+  CardBody,
+  Heading,
+  Text,
+  CardFooter,
+  Button,
+} from "@chakra-ui/react";
+
+function Payments() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://trevelioussite.onrender.com/destination");
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
-    <div>Payments</div>
-  )
+    <div>
+      {data.map((item) => (
+        <Card
+          key={item.id}
+          direction={{ base: "column", sm: "row" }}
+          overflow="hidden"
+          variant="outline"
+        >
+          <Image
+            objectFit="cover"
+            maxW={{ base: "100%", sm: "200px" }}
+            src={item.url}
+            alt={item.name}
+          />
+
+          <Stack>
+            <CardBody>
+              <Heading size="md">{item.name}</Heading>
+
+              <Text py="2">{item.description}</Text>
+            </CardBody>
+
+            <CardFooter>
+              <Button variant="solid" colorScheme="white" bg="#ff6347">
+                {/* Book{item.name} */}
+                Select Room
+              </Button>
+            </CardFooter>
+          </Stack>
+        </Card>
+      ))}
+    </div>
+  );
 }
 
-export default Payments
+export default Payments;
